d28a521ef2a293f67e389504eb655dda6c89daa4
taylornelms15 <taylornelms15@gmail.com>
2018-05-03 06:43:04 -0400

    Continued ability implementation

--- MTGDecker/AbilityParameter_Functions.swift
+++ MTGDecker/AbilityParameter_Functions.swift
@@ -224,4 +224,206 @@ extension AbilityParameter{
         return Set<FieldState>([resultStateW, resultStateU, resultStateB, resultStateR, resultStateG, resultStateC])
     }//addAny
     
+    /**
+     Executes a given parameter of an ability and returns a set of possible states after that parameter's execution.
+     
+     Relevant ability text: {W}
+     
+     - parameter currentState: The current state of the playing field
+     - parameter location: The location of the card whose ability we are activating
+     - returns: Set of all possible field states after execution of ability parameter. If execution is impossible for whatever reason (such as if a cost cannot be paid, or no valid target exists), returns nil.
+     
+     */
+    static func costW(currentState: FieldState, location: CardLocation) -> Set<FieldState>?{
+        if currentState.manaPool.w <= 0{
+            return nil
+        }
+        let resultState = FieldState(state: currentState)
+        
+        resultState.manaPool.w -= 1
+        
+        return Set<FieldState>([resultState])
+    }//costW
+    
+    /**
+     Executes a given parameter of an ability and returns a set of possible states after that parameter's execution.
+     
+     Relevant ability text: {U}
+     
+     - parameter currentState: The current state of the playing field
+     - parameter location: The location of the card whose ability we are activating
+     - returns: Set of all possible field states after execution of ability parameter. If execution is impossible for whatever reason (such as if a cost cannot be paid, or no valid target exists), returns nil.
+     
+     */
+    static func costU(currentState: FieldState, location: CardLocation) -> Set<FieldState>?{
+        if currentState.manaPool.u <= 0{
+            return nil
+        }
+        let resultState = FieldState(state: currentState)
+        
+        resultState.manaPool.u -= 1
+        
+        return Set<FieldState>([resultState])
+    }//costU
+    
+    /**
+     Executes a given parameter of an ability and returns a set of possible states after that parameter's execution.
+     
+     Relevant ability text: {B}
+     
+     - parameter currentState: The current state of the playing field
+     - parameter location: The location of the card whose ability we are activating
+     - returns: Set of all possible field states after execution of ability parameter. If execution is impossible for whatever reason (such as if a cost cannot be paid, or no valid target exists), returns nil.
+     
+     */
+    static func costB(currentState: FieldState, location: CardLocation) -> Set<FieldState>?{
+        if currentState.manaPool.b <= 0{
+            return nil
+        }
+        let resultState = FieldState(state: currentState)
+        
+        resultState.manaPool.b -= 1
+        
+        return Set<FieldState>([resultState])
+    }//costB
+    
+    /**
+     Executes a given parameter of an ability and returns a set of possible states after that parameter's execution.
+     
+     Relevant ability text: {R}
+     
+     - parameter currentState: The current state of the playing field
+     - parameter location: The location of the card whose ability we are activating
+     - returns: Set of all possible field states after execution of ability parameter. If execution is impossible for whatever reason (such as if a cost cannot be paid, or no valid target exists), returns nil.
+     
+     */
+    static func costR(currentState: FieldState, location: CardLocation) -> Set<FieldState>?{
+        if currentState.manaPool.r <= 0{
+            return nil
+        }
+        let resultState = FieldState(state: currentState)
+        
+        resultState.manaPool.r -= 1
+        
+        return Set<FieldState>([resultState])
+    }//costR
+    
+    /**
+     Executes a given parameter of an ability and returns a set of possible states after that parameter's execution.
+     
+     Relevant ability text: {G}
+     
+     - parameter currentState: The current state of the playing field
+     - parameter location: The location of the card whose ability we are activating
+     - returns: Set of all possible field states after execution of ability parameter. If execution is impossible for whatever reason (such as if a cost cannot be paid, or no valid target exists), returns nil.
+     
+     */
+    static func costG(currentState: FieldState, location: CardLocation) -> Set<FieldState>?{
+        if currentState.manaPool.g <= 0{
+            return nil
+        }
+        let resultState = FieldState(state: currentState)
+        
+        resultState.manaPool.g -= 1
+        
+        return Set<FieldState>([resultState])
+    }//costG
+    
+    /**
+     Executes a given parameter of an ability and returns a set of possible states after that parameter's execution.
+     
+     Relevant ability text: {W}
+     
+     - parameter currentState: The current state of the playing field
+     - parameter location: The location of the card whose ability we are activating
+     - returns: Set of all possible field states after execution of ability parameter. If execution is impossible for whatever reason (such as if a cost cannot be paid, or no valid target exists), returns nil.
+     
+     */
+    static func costC(currentState: FieldState, location: CardLocation) -> Set<FieldState>?{
+        if currentState.manaPool.c <= 0{
+            return nil
+        }
+        let resultState = FieldState(state: currentState)
+        
+        resultState.manaPool.c -= 1
+        
+        return Set<FieldState>([resultState])
+    }//costC
+    
+    /**
+     Executes a given parameter of an ability and returns a set of possible states after that parameter's execution.
+     
+     Relevant ability text: {1}
+     
+     - parameter currentState: The current state of the playing field
+     - parameter location: The location of the card whose ability we are activating
+     - returns: Set of all possible field states after execution of ability parameter. If execution is impossible for whatever reason (such as if a cost cannot be paid, or no valid target exists), returns nil.
+     
+     */
+    static func costAny(currentState: FieldState, location: CardLocation) -> Set<FieldState>?{
+        
+        var resultStates: Set<FieldState> = Set<FieldState>()
+        
+        if currentState.manaPool.w > 0{
+            let resultState: FieldState = FieldState(state: currentState)
+            resultState.manaPool.w -= 1
+            resultStates.insert(resultState)
+        }
+        if currentState.manaPool.u > 0{
+            let resultState: FieldState = FieldState(state: currentState)
+            resultState.manaPool.u -= 1
+            resultStates.insert(resultState)
+        }
+        if currentState.manaPool.b > 0{
+            let resultState: FieldState = FieldState(state: currentState)
+            resultState.manaPool.b -= 1
+            resultStates.insert(resultState)
+        }
+        if currentState.manaPool.r > 0{
+            let resultState: FieldState = FieldState(state: currentState)
+            resultState.manaPool.r -= 1
+            resultStates.insert(resultState)
+        }
+        if currentState.manaPool.g > 0{
+            let resultState: FieldState = FieldState(state: currentState)
+            resultState.manaPool.g -= 1
+            resultStates.insert(resultState)
+        }
+        if currentState.manaPool.c > 0{
+            let resultState: FieldState = FieldState(state: currentState)
+            resultState.manaPool.c -= 1
+            resultStates.insert(resultState)
+        }
+        
+        
+        if resultStates.count > 0{
+            return resultStates
+        }
+        return nil
+    }//costAny
+    
+    /**
+     Executes a given parameter of an ability and returns a set of possible states after that parameter's execution.
+     
+     Relevant ability text: Sacrifice [cardname]
+     
+     - parameter currentState: The current state of the playing field
+     - parameter location: The location of the card whose ability we are activating
+     - returns: Set of all possible field states after execution of ability parameter. If execution is impossible for whatever reason (such as if a cost cannot be paid, or no valid target exists), returns nil.
+     
+     */
+    static func sacrifice(currentState: FieldState, location: CardLocation) -> Set<FieldState>?{
+        if location.location != .battlefield || location.index == nil || location.index! < 0 || location.index! >= currentState.battlefield.count{
+            return nil
+        }//Cannot sacrifice something if it's not on the battlefield, or if we're pointing at a location nowhere close to the battlefield
+        
+        let myNewState = FieldState(state: currentState)
+        let myCard: FieldState.FieldCard = myNewState.battlefield[location.index!]
+        
+        myNewState.battlefield.remove(at: location.index!)
+        myNewState.graveyard.append(myCard.card)
+        
+        return Set<FieldState>([myNewState])
+    }//sacrifice
+    
 }//AbilityParameter_Functions
